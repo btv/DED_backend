c0e5ad9ea67696b85aa86f9f9d75cde9ced6fe43
Bryce Verdier <bryceverdier@gmail.com>
2020-02-22 12:03:43 -0700

    Initial Commit. Working index displays

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#[macro_use]
+extern crate actix_web;
+
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/{name}/index.html")]
+async fn index(info: web::Path<String>) -> impl Responder {
+    format!("Hello {}!", info)
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(web::resource("/").to(|| async {"Hello World!"}))
+        })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
