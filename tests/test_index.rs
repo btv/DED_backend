42545319a0fd9ae277f69221ab11e3bba8e7ac90
Bryce Verdier <bryceverdier@gmail.com>
2020-02-27 14:58:54 -0700

    fixed broken tests.

--- tests/test_index.rs
+++ tests/test_index.rs
@@ -2,13 +2,13 @@
 
 mod tests {
     use DED_backend::handlers::index::{index_with_name,index};
-    use actix_web::{test, App};
+    use actix_web::{web, test, App};
 
     #[actix_rt::test]
     async fn test_index_with_name_get() {
         let mut app = test::init_service(
             App::new()
-                .service(index_with_name)
+                .route("/{name}/index.html", web::get().to(index_with_name))
         )
         .await;
         let req = test::TestRequest::with_header("content-type", "text/plain")
@@ -22,7 +22,7 @@ mod tests {
     async fn test_index_get() {
         let mut app = test::init_service(
             App::new()
-                .service(index)
+                .route("/", web::get().to(index))
         )
         .await;
         let req = test::TestRequest::with_header("content-type", "text/plain")
