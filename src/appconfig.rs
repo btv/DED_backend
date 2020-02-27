de41eb2b4a77606409d38365774e5034ec8e42a9
Bryce Verdier <bryceverdier@gmail.com>
2020-02-27 14:27:44 -0700

    Changes:
    - put Cargo.lock into gitignore. That should be local only.
    - modified the index fuctions. The get macro was messing with the
    docstrings.
    
    Now all documentation works.

--- src/appconfig.rs
+++ src/appconfig.rs
@@ -2,6 +2,11 @@ use actix_web::web;
 
 use crate::handlers::index::{index, index_with_name};
 
-pub fn config_app(cfg: &mut web::ServiceConfig) {
-    cfg.service(index).service(index_with_name);
+
+/// This function is modified as needed to make sure that all of the
+/// applicaitons routeing happens.
+pub fn config_app(cfg: &mut web::ServiceConfig) ->  &mut web::ServiceConfig {
+    cfg
+        .route("/", web::get().to(index))
+        .route("/{name}/index.html", web::get().to(index_with_name))
 }
--- src/handlers/index.rs
+++ src/handlers/index.rs
@@ -1,17 +1,24 @@
 //! Handler for all the of the index based urls.
 //!
 //! This library is mostly provided as an example of both layout of
-//! how the future modules should be done and how url's within thos modules
+//! how the future modules should be done and how url's within those modules
 //! should look.
-//!
-use actix_web::{get, web, Responder};
 
-#[get("/{name}/index.html")]
+use actix_web::{web, Responder};
+
+/// Expiremental page, pulling the name parameter from the URL and using
+/// it for the output string.
+///
+/// **Arguments**:
+/// * name: <pretty self descriptive
+///
+/// **Returns**:
+/// A implementation of a `Responder` with the text `Hello [name]` to the person called on.
 pub async fn index_with_name(info: web::Path<String>) -> impl Responder {
     format!("Hello {}!", info)
 }
 
-#[get("/")]
+/// Returns the information needed for the index page.
 pub async fn index() -> impl Responder {
     "Hello World!".to_string()
 }
