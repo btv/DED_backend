cfdb38dd2bf4872cc3414868db58603f83ac406e
Bryce Verdier <bryceverdier@gmail.com>
2020-02-27 14:48:23 -0700

    fixed compile error.

--- src/appconfig.rs
+++ src/appconfig.rs
@@ -5,8 +5,8 @@ use crate::handlers::index::{index, index_with_name};
 
 /// This function is modified as needed to make sure that all of the
 /// applicaitons routeing happens.
-pub fn config_app(cfg: &mut web::ServiceConfig) ->  &mut web::ServiceConfig {
+pub fn config_app(cfg: &mut web::ServiceConfig) {
     cfg
         .route("/", web::get().to(index))
-        .route("/{name}/index.html", web::get().to(index_with_name))
+        .route("/{name}/index.html", web::get().to(index_with_name));
 }
