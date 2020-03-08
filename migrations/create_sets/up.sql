4bf25e3c7889c4f7cb95ee901f6bb3ed5ee0aa02
Bryce Verdier <bryceverdier@gmail.com>
2020-03-08 14:17:34 -0700

    changed type to style do to keyword issue.

--- migrations/create_sets/up.sql
+++ migrations/create_sets/up.sql
@@ -1,12 +1,14 @@
+Create TYPE style as ENUM ('normal', 'warmup', 'cooldown', 'drop', 'fail')
+
 CREATE TABLE sets {
     id integer PRIMARY KEY,
     exercise_id int null,
-    type varchar null,
+    style style,
     unit varchar null,
     goal_reps smallint,
     goal_value varchar,
     description varchar,
-    created_or_completed
+    created_or_completed timestamp,
     completed_reps smallint null,
     completed_value varchar null
 }
