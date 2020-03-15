5af6aee94b865a7cfbd31b182666ccc55c3ef4ff
Bryce Verdier <bryceverdier@gmail.com>
2020-03-15 15:21:50 -0600

    new set is for sets that haven't been uploaded to the database yet, for serialization from the client.

--- src/models/sets.rs
+++ src/models/sets.rs
@@ -20,14 +20,12 @@ pub struct Set {
 #[derive(Debug, Clone, Serialize, Deserialize, Queryable, Insertable)]
 #[table_name="sets"]
 pub struct NewSet {
-    pub id: i32,
     pub exercise_id: i32,
     pub style: String,
     pub unit: String,
     pub goal_reps: i16,
     pub goal_value: String,
     pub description: String,
-    pub created_or_completed: SystemTime,
     pub completed_reps: i16,
     pub completed_value: String,
 }
