ca569aed014118636cc28c15a1dedc40f37583d8
Bryce Verdier <bryceverdier@gmail.com>
2020-03-15 10:21:06 -0600

    putting traits with datatypes.

--- src/handlers/set.rs
+++ src/handlers/set.rs
@@ -4,90 +4,9 @@ use diesel::prelude::*;
 use actix_web::{web, Responder};
 use std::time::SystemTime;
 
-use crate::models::Set;
+use crate::models::sets::{Set,NewSet};
 
-impl Set {
-    //! Function to create a new Set instance.
-    fn new(
-        exercise_id: i32,
-        style: String,
-        unit: String,
-        goal_reps: i16,
-        goal_value: String,
-        description: String,
-        completed_reps: i16,
-        completed_value: String,
-    ) -> Set {
-        Set {
-            id: rand::random::<i32>().abs(),
-            exercise_id: exercise_id,
-            unit: unit,
-            style: style,
-            goal_reps: goal_reps,
-            goal_value: goal_value,
-            description: description,
-            created_or_completed: SystemTime::now(),
-            completed_reps: completed_reps,
-            completed_value: completed_value
-        }
-    }
-}
-
-pub async fn new_set(
-        exercise_id: i32,
-        style: String,
-        unit: String,
-        goal_reps: i16,
-        goal_value: String,
-        description: String,
-        completed_reps: i16,
-        completed_value: String,
-    ) -> impl Responder {
-
-        let new_set = Set::new(
-            exercise_id,
-            style,
-            unit,
-            goal_reps,
-            goal_value,
-            description,
-            completed_reps,
-            completed_value
-        );
-
-        //diesel::insert_into(sete).values(&new_new).execute(conn)?;
-
-        web::Json(new_set.id)
-}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use num_traits::sign;
-
-    #[test]
-    fn new_set_unit_test() {
-        let new_test_set = Set::new(
-            -1,
-            "none".to_string(),
-            "none".to_string(),
-            10,
-            "none".to_string(),
-            "none".to_string(),
-            0,
-            "none".to_string()
-        );
-
-        // String tests
-        assert_eq!(new_test_set.style, "none".to_string());
-        assert_eq!(new_test_set.unit, "none".to_string());
-        assert_eq!(new_test_set.description, "none".to_string());
-        assert_eq!(new_test_set.goal_value, "none".to_string());
-        assert_eq!(new_test_set.completed_value, "none".to_string());
-
-        //Numerical tests
-        assert_eq!(new_test_set.completed_reps, 0);
-        assert_eq!(new_test_set.goal_reps, 10);
-        assert_eq!(true, sign::Signed::is_positive(&new_test_set.id));
-    }
 }
--- src/models/users.rs
+++ src/models/users.rs
@@ -21,3 +21,26 @@ pub struct NewUser<'a>{
     pub email: &'a str,
     pub salt: &'a str,
 }
+
+impl User {
+    pub fn get_user<'a>(conn: &PgConnection, id: &'a i32) -> Result<User, diesel::result::Error> {
+        use schema::users;
+        users::table.find(id).get_result::<User>(conn)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct UserList(pub Vec<User>);
+
+impl UserList {
+    pub fn get_users(conn: &PgConnection) -> UserList {
+        use crate::schema::users::dsl::*;
+
+
+        let results = users
+            .load::<User>(conn)
+            .expect("Error retreiving users");
+
+        UserList(results)
+    }
+}
