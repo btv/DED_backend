20c615b239be64f3faa66887968a41caec59dc95
Bryce Verdier <bryceverdier@gmail.com>
2020-03-15 12:24:13 -0600

    moved user creating test to file. Removed code for integration tests until handlers are built.

--- src/models/users.rs
+++ src/models/users.rs
@@ -1,4 +1,8 @@
 use serde::{Serialize, Deserialize};
+#[cfg(test)]
+use mockall::*;
+#[cfg(test)]
+use mockall::predicate::*;
 
 use crate::schema::users;
 
@@ -25,10 +29,14 @@ pub struct NewUser {
 pub struct UserList(pub Vec<User>);
 
 impl User {
-    //pub fn get_user<'a>(id: &'a i32) -> Result<User, diesel::result::Error> {
-    //    use crate::schema::users;
-    //    users::table.find(id).get_result::<User>(conn)
-    //}
+    pub fn get_user (id: i32) -> Result<User, diesel::result::Error> {
+        use diesel::QueryDsl;
+        use diesel::RunQueryDsl;
+        use crate::establish_connection;
+
+        let conn = establish_connection();
+        users::table.find(id).get_result(&conn)
+    }
 
     pub fn create(&self) -> Result<User, diesel::result::Error> {
         use diesel::RunQueryDsl;
@@ -52,3 +60,37 @@ impl User {
 //        UserList(results)
 //    }
 //}
+//
+//
+//
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_user_structure() {
+
+
+        let t_id = 1999;
+        let t_uname = "TestUser";
+        let t_fname = "Usable User";
+        let t_email = "testuser@testdomain.com";
+        let t_salt = "some_like_MSG";
+
+        let t_user = User {
+            id: t_id,
+            username: t_uname.to_string(),
+            fname: t_fname.to_string(),
+            email: t_email.to_string(),
+            salt: t_salt.to_string()
+        };
+
+        assert_eq!(t_id, t_user.id);
+        assert_eq!(t_uname, t_user.username);
+        assert_eq!(t_fname, t_user.fname);
+        assert_eq!(t_email, t_user.email);
+        assert_eq!(t_salt, t_user.salt);
+
+    }
+}
--- tests/integration_test_database.rs
+++ tests/integration_test_database.rs
@@ -4,12 +4,6 @@ extern crate diesel;
 extern crate dotenv;
 
 
-
-
-
-
-
-
 mod tests {
     use DED_backend::establish_connection;
     use DED_backend::models::users::User;
@@ -18,61 +12,10 @@ mod tests {
 
 
     #[test]
-    fn test_db_insert_and_find() {
-
-
-        let conn = establish_connection();
-
-        let _xxx = diesel::delete(DED_backend::schema::users::dsl::users)
-            .execute(&conn);
-
-        // assert_eq!(xxx,Ok(0));
-
-        let t_id = 1999;
-        let t_uname = "TestUser";
-        let t_fname = "Usable User";
-        let t_email = "testuser@testdomain.com";
-        let t_salt = "some_like_MSG";
-
-        let t_user = create_user(&t_id, &t_uname, &t_fname, &t_email, &t_salt);
-
-        assert_eq!(t_id, t_user.id);
-        assert_eq!(t_uname, t_user.username);
-        assert_eq!(t_fname, t_user.fname);
-        assert_eq!(t_email, t_user.email);
-        assert_eq!(t_salt, t_user.salt);
-
-        let user_result = User::get_user(&conn, &t_id);
-
-        match user_result{
-            Ok(q_user) => {
-                assert_eq!(t_id, q_user.id);
-                assert_eq!(t_uname, q_user.username);
-                assert_eq!(t_fname, q_user.fname);
-                assert_eq!(t_email, q_user.email);
-                assert_eq!(t_salt, q_user.salt);
-            }
-            Err(_e) => {
-                assert_eq!(5,2);
-            }
-        }
-
+    fn test_user_insert() {
     }
+
     #[test]
-    fn test_db_not_found() {
-
-        let conn = establish_connection();
-        let t_id = -123456;
-
-        let user_result = User::get_user(&t_id);
-
-        match user_result{
-            Ok(q_user) => {
-                assert_eq!(t_id, q_user.id);
-            }
-            Err(e) => {
-                assert_eq!(e,diesel::NotFound);
-            }
-        }
+    fn test_user_find() {
     }
 }
