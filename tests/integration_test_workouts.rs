6b2c748346763e888bf4f59567d0f9bb169ca7c5
Dwight Browne <dwight@dwightjbrowne.com>
2020-03-19 18:14:50 -0600

    fix to last test

--- tests/integration_test_workouts.rs
+++ tests/integration_test_workouts.rs
@@ -80,8 +80,15 @@ mod tests {
                 assert_eq!(r.exercise, t_exercise);
                 assert_eq!(r.description, t_description);
                 assert_eq!(r.notes, t_notes);
-                assert_eq!(r.created_time, t_created_time);
-                assert_eq!(r.completed_time, t_completed_time);
+                let mut sec_original = t_created_time.duration_since(UNIX_EPOCH).unwrap().as_secs();
+                let mut sec_saves = r.created_time.duration_since(UNIX_EPOCH).unwrap().as_secs();
+                assert_eq!(sec_original, sec_saves);
+
+                sec_original = t_completed_time.duration_since(UNIX_EPOCH).unwrap().as_secs();
+                sec_saves = r.completed_time.duration_since(UNIX_EPOCH).unwrap().as_secs();
+                assert_eq!(sec_original, sec_saves);
+
+
                 assert_eq!(r.create_id, t_create_id);
                 assert_eq!(r.completed_id, t_completed_id);
             }
