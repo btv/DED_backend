733f7e8d7d21d5034d5c7e91050474cb13c5ed90
Bryce Verdier <bryceverdier@gmail.com>
2020-03-28 21:08:15 -0600

    commented unused imports, will add them later.

--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -1,6 +1,6 @@
 
 
-use actix_web::{web, Responder, HttpResponse};
-use crate::{establish_connection,hash_secret_key};
-use crate::models::users::{NewUser,User};
+//use actix_web::{web, Responder, HttpResponse};
+//use crate::{establish_connection,hash_secret_key};
+//use crate::models::users::{NewUser,User};
 
