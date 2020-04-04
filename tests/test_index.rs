18a60dd3daf50381b61773ea29e2f8f6fb3d17ed
Bryce Verdier <bryceverdier@gmail.com>
2020-04-04 16:08:36 -0600

    added initial framework for testing. Still working on it.

--- tests/test_index.rs
+++ tests/test_index.rs
@@ -1,6 +1,6 @@
 mod tests {
     use DED_backend::handlers::index;
-    use DED_backend::models::users::{NewUser, SlimUser};
+    use DED_backend::models::users::{NewUser, SlimUser, AuthData};
     use actix_web::{web, test, App, http::header};
 
 
@@ -42,6 +42,34 @@ mod tests {
                       serde_json::to_string(&new_user).unwrap()
                   )
                   .to_request();
+
+
+        let resp_user: SlimUser = test::read_response_json(&mut app, req).await;
+        assert!(resp_user == new_user);
+    }
+
+    #[actix_rt::test]
+    async fn test_login() {
+        let mut app = test::init_service(
+            App::new()
+                .route("/login/", web::post().to(index::login))
+        )
+        .await;
+
+        let new_user = AuthData {
+            username: "bob1984".to_string(),
+            password: "Sup3rS3cr3tP@ssw@ord".to_string()
+        };
+
+        let req = test::TestRequest::post()
+                  .header(header::CONTENT_TYPE, "application/json")
+                  .uri("/login/")
+                  .set_payload(
+                      serde_json::to_string(&new_user).unwrap()
+                  )
+                  .to_request();
+
+        println!("{:?}", req);
 
 
         let resp_user: SlimUser = test::read_response_json(&mut app, req).await;
