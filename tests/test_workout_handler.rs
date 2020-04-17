8f98f57a273b0e73eb52eb123dec9df9879148f3
Bryce Verdier <bryceverdier@gmail.com>
2020-04-17 14:38:00 -0600

    add test for getting workout by id.

--- tests/test_workout_handler.rs
+++ tests/test_workout_handler.rs
@@ -30,5 +30,46 @@ mod tests {
         let resp_wk: Workout = test::read_response_json(&mut app, req).await;
         assert!(resp_wk == payload);
     }
+
+    #[actix_rt::test]
+    async fn test_find_by_workout_id() {
+        use DED_backend::establish_connection;
+
+        let conn = establish_connection().get().unwrap();
+
+        let mut app = test::init_service(
+            App::new()
+                .route("/workouts/{id}/", web::get().to(workout::find_by_id))
+        )
+        .await;
+
+        let test_find = NewWorkout {
+            origin_id: 66,
+            exercise: 4,
+            fname: "working hard".to_string(),
+            description: "it has a nice flaky crust and good flavor.".to_string(),
+            notes: "F".to_string(),
+        };
+
+        let test_find_results = test_find.create(&conn).unwrap();
+
+        let req = test::TestRequest::get()
+            .header(header::CONTENT_TYPE, "application/json")
+            .uri(format!("/workouts/{}/", test_find_results.id).as_str())
+            .to_request();
+
+        let resp = test::call_service(&mut app, req).await;
+        match resp.status().is_success() {
+            false => (),
+            true => {
+                let n_req = test::TestRequest::get()
+                    .header(header::CONTENT_TYPE, "application/json")
+                    .uri(format!("/workouts/{}/", test_find_results.id).as_str())
+                    .to_request();
+                let new_resp: Workout = test::read_response_json(&mut app, n_req).await;
+                assert!(new_resp == test_find);
+            }
+        }
+    }
 }
 
